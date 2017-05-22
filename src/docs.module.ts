ec43b564b21926a69a60e1a36dc8cb0aa6abf4e5
Diabeu <scibor.rudnicki@ster-project.pl>
2017-05-22 17:55:06 +0200

    refactor(module): change Prism import

--- src/docs.module.ts
+++ src/docs.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@angular/material';
 
-import { PrismModule } from '@ngx-prism/core';
+import PrismModule from '@ngx-prism/core';
 
 import { DocsApiComponent } from './api';
 import { DocsExampleComponent } from './example';
