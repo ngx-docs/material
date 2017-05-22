2e387093c2bf6844342dc670f011af9ed47cb426
Diabeu <scibor.rudnicki@ster-project.pl>
2017-05-22 17:49:13 +0200

    chore: add plugin angular, html, and nodeResolve es2015, main

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
-
-import nodeResolve  from 'rollup-plugin-node-resolve';
-import commonjs     from 'rollup-plugin-commonjs';
-import uglify       from 'rollup-plugin-uglify';
+import angular from 'rollup-plugin-angular';
+import commonjs from 'rollup-plugin-commonjs';
+import html from 'rollup-plugin-html';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'dist/index.js',
@@ -12,17 +14,45 @@ export default {
   moduleName: '@ngx-docs/material',
   onwarn,
   plugins: [
-      commonjs({
-          include: 'node_modules/rxjs/**',
-      }),
-      nodeResolve({ jsnext: true, module: true }),
-      uglify()  
+    angular(),
+    commonjs({
+      namedExports: {
+        'node_modules/rxjs/**': ['named']
+      }
+    }),
+    html({
+      include: '**/*.html'
+    }),
+    nodeResolve({
+      // use "es2015" field for ES2015 modules with ES2015 code,
+	    // if possible
+      es2015: true, // Default: false
+
+      // use "module" field for ES2015 modules with ES5 code,
+      // if possible
+      module: true, // Default: true
+
+      // use "jsnext:main" if possible
+      // – see https://github.com/rollup/rollup/wiki/jsnext:main
+      jsnext: true,  // Default: false
+
+      // use "main" field or index.js, even if it's not an ES6 module
+      // (needs to be converted from CommonJS to ES6
+      // – see https://github.com/rollup/rollup-plugin-commonjs
+      main: true,  // Default: true
+
+      extensions: [ '.js', '.json', 'html']
+    }),
+    typescript({
+      typescript: require('./node_modules/typescript')
+    }),
+    uglify()  
   ],
   globals: {
     '@angular/animations': 'ng.animations',
     '@angular/core': 'ng.core',
     '@angular/forms': 'ng.forms',
-    '@angular/http': 'ng.http',
+    '@angular/http': 'ng.http'
   }
 };
 
