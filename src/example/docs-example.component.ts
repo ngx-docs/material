25f01b4cce11155776069a2b7f0efdd132edce5e
Diabeu <scibor.rudnicki@ster-project.pl>
2017-05-25 17:51:53 +0200

    refactor(DocsExampleComponent): remove @Input properties data and body, ViewChild body

--- src/example/docs-example.component.ts
+++ src/example/docs-example.component.ts
@@ -1,5 +1,6 @@
 // angular core
 import { Component, ElementRef, Input, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+
 // get default features for component
 import { DocsClass } from './../docs.class';
 // import template from html file
@@ -12,14 +13,10 @@ import template from './docs-example.component.html';
 })
 export class DocsExampleComponent extends DocsClass implements OnInit {
 
-  @Input() public body: string;
   @Input('css') public css: string;
-  @Input('data') public data: any;
   @Input('html') public html: string;
   @Input('title') public title: string;
   @Input('ts') public ts: string;
-
-  @ViewChild('body') setBody: ElementRef;
 
   constructor() {
     super();
