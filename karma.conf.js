73f1b0c6b06e090a533ede129fa67d326688f61c
Diabeu <scibor.rudnicki@ster-project.pl>
2017-07-18 18:02:20 +0200

    test: use karma-rollup-preprocessor instead of plugin - deprecated

--- karma.conf.js
+++ karma.conf.js
@@ -13,14 +13,13 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-
     // list of files / patterns to load in the browser
     files: [
+      'test/index.ts',
       'test/*.ts',
       'src/*.spec.ts',
       'src/**/*.spec.ts'
@@ -29,7 +28,7 @@ module.exports = function(config) {
     plugins: [
       require('karma-coverage'),
       require('karma-chrome-launcher'),
-      require('karma-rollup-plugin'),
+      require('karma-rollup-preprocessor'),
       require('karma-jasmine')
     ],    
 
@@ -39,6 +38,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'test/index.ts': ['rollup'],
       'test/*.ts': ['rollup'],
       'src/*.spec.ts': ['rollup'],
       'src/**/*.spec.ts': ['rollup']
@@ -47,7 +47,7 @@ module.exports = function(config) {
     rollupPreprocessor: {
       context: 'this',
       // will help to prevent conflicts between different tests entries
-      moduleName: '@ngx-docs/material',
+      moduleName: 'ngxdocsmaterial',
       format: 'iife',
       sourceMap: 'inline',
       // rollup settings. See Rollup documentation
@@ -95,19 +95,15 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
-
     // web server port
     port: 9876,
-
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
